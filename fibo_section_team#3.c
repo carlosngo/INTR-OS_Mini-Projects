f2940ab9fe3c826ba863aac78bb5d504083a8c86
Jan-Ren <46782934+Jan-Ren@users.noreply.github.com>
2019-07-03 17:23:57 +0800

    Update fibo_section_team#3.c

--- fibo_section_team#3.c
+++ fibo_section_team#3.c
@@ -24,7 +24,7 @@ int compute_fibo (int n1, int n2) {
 
 int write_fibo (int fibnum) {
     FILE* in = fopen("fibo.txt", "a");
-    frprintf(in, "\n" + fibnum);
+    frprintf(in, "\n%d", fibnum);
     fclose(in);
 }
 
