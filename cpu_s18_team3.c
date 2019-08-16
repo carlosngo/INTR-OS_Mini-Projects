8a1c131b40f1b154511fc466019bce4bd059853d
Carlos <carlos_ngo@dlsu.edu.ph>
2019-08-16 12:00:09 +0800

    Add files via upload

---
+++ cpu_s18_team3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+#define MAX_NUM_OF_PROCESSES 5
+
+typedef struct {
+    char p_id;
+    int arrival;
+    int remaining;
+} process;
+
+
+void read_file(process list[]);
+void print_fcfs(process list[]);
+void print_sjf(process list[]);
+void print_srtf(process list[]);
+void print_rr(process list[], int q);
+
+int
+main() {
+    
+    process list[MAX_NUM_OF_PROCESSES];
+    read_file(list);
+    print_fcfs(list);
+    print_sjf(list);
+    print_srtf(list);
+    print_rr(list);
+    return 0;
+}
+
